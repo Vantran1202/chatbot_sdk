1b0dfa98488c1074ecec23f6a4807162e37ddf94
levan <levan@gmail.com>
2023-10-26 00:26:27 +0700

    Config Stimulus and bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload';
+import StimulusHMR from 'vite-plugin-stimulus-hmr';
 
 export default defineConfig({
+  clearScreen: false,
   plugins: [
     RubyPlugin(),
+    FullReload(["config/routes.rb", "app/views/**/*"], { delay: 200 }),
+    StimulusHMR()
   ],
+  root: "./app/javascript",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "/app/javascript/entrypoints/application.js"
+    }
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': './node_modules/bootstrap'
+    }
+  }
 })
