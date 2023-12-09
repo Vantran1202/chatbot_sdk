15245ebaa46f09b8d4bf1b74ba3c38e8bc93285b
tm-minhtang <minh.tang1@tomosia.com>
2023-12-09 23:36:33 +0700

    Add route campaigns

---
+++ app/controllers/authenticate_controller.rb
@@ -0,0 +1,2 @@
+class AuthenticateController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :home, only: %i[index]
+  scope :projects do
+    resources :campaigns, only: %i[index]
+  end
 end
